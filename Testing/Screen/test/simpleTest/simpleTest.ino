a5058f584d49492b8d52b8c295e331a9232fd3d9
hmerrell <hannamerrell18@gmail.com>
2026-04-09 19:28:53 -0600

    initial testing

---
+++ Testing/Screen/test/simpleTest/simpleTest.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <DIYables_OLED_SSD1309.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESSS 0x3c     // initialize the SSD1309 - use the address 0x3c (default for most modules)
+
+DIYables_OLED_SSD1309 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // create display object (pass -1 when no reset pin detected)
+
+void setup() {
+  Serial.begin(9600);
+ 
+  if (!display.begin(SSD1309_SWITCHCAPVCC, SCREEN_ADDRESSS)) {
+    Serial.println(F("SSD1309 allocation failed"));
+    for (;;); // basically an infinite loop, functions same as a while(true) loop
+  }
+
+  display.clearDisplay();
+
+  display.setTextSize(1);         // 6x8 pixels per character
+  display.setTextColor(SSD1309_PIXEL_ON);       // turn pixels on
+  display.setCursor(0, 0);
+  display.println(F("Hello world"));
+  display.println();
+  display.setTextSize(2);         // 12x16 pixels per character
+  display.println(F("DIYables"));
+  display.setTextSize(1);
+  display.println();
+  display.println(F("SSD1309 OLED 128x64"));
+  display.display();              // push buffer to screen
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

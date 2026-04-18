f4c7eac24f3a76b27e7296201eb8f111df40f558
hmerrell <hannamerrell18@gmail.com>
2026-04-18 17:37:56 -0600

    adding modularity

---
+++ Testing/WeatherStation/displayFunctions.h
@@ -0,0 +1,7 @@
+#ifndef DISPLAY_FUNCTIONS_H
+#define DISPLAY_FUNCTIONS_H
+
+void writeWeatherStationDisplay(float tempF, float pressure_hPa, float humidity);
+void writeStationWelcome();
+
+#endif
---
+++ Testing/WeatherStation/globals.h
@@ -0,0 +1,9 @@
+//Declaration of shared constants and global objects
+
+#ifndef DISPLAYFUNCTIONS_H
+#define DISPLAYFUNCTIONS_H
+
+void showStartupScreen();
+void drawWeatherScreen(float tempF, float pressure, float humidity);
+
+#endif

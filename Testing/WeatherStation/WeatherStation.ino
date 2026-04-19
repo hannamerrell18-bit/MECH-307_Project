a992c4aa2c3bf127a275c2d7935aea655f299783
hmerrell <hannamerrell18@gmail.com>
2026-04-19 03:48:46 -0600

    organizing

--- Testing/WeatherStation/WeatherStation.ino
+++ Testing/WeatherStation/WeatherStation.ino
@@ -4,65 +4,43 @@
 #include <Adafruit_GFX.h>
 #include <DIYables_OLED_SSD1309.h>
 
-#define TCAADDR 0x70
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-#define BME_ADDRESS 0x77
-Adafruit_BME280 bme; //create BME 280 sensor object
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-#define SCREEN_ADDRESSS 0x3c     // initialize the SSD1309 - use the address 0x3c (default for most modules)
-DIYables_OLED_SSD1309 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // create display object (pass -1 when no reset pin detected)
-
-void writeWeatherStationDisplay(float tempF, float pressure_hPa, float humidity);
-void writeStationWelcome();
-
-void tcaselect(uint8_t i) {
-  if (i > 7) return;
-
-  Wire.beginTransmission(TCAADDR);
-  Wire.write(1 << i);
-  Wire.endTransmission();
-}
+#include "globals.h"
+#include "mux_functions.h"
+#include "display_functions.h"
 
 void setup() {
-  // put your setup code here, to run once:
- Serial.begin(115200);
- Wire.begin(); //fail safe for beginning I2C comm; most libraries have it, but good habit to use in code
- 
- tcaselect(2);
- if (!display.begin(SSD1309_SWITCHCAPVCC, SCREEN_ADDRESSS)) {
-  Serial.println(F("SSD1309 allocation failed"));
-  while(1); // basically an infinite loop, functions same as a while(true) loop
- }
+  Serial.begin(115200);
+  Wire.begin();
 
- tcaselect(3);
- if(!bme.begin(BME_ADDRESS)) {   // default I2C address is 0x76
-  Serial.println("Could not find valid BME280 sensor, check wiring!");
-  while(1);
- }
+  tcaselect(2);
+  if (!display.begin(SSD1309_SWITCHCAPVCC, SCREEN_ADDRESSS)) {
+    Serial.println(F("SSD1309 allocation failed"));
+    while (1);
+  }
 
- tcaselect(2);
- display.clearDisplay();
- display.display();
- delay(3000);
+  tcaselect(3);
+  if (!bme.begin(BME_ADDRESS)) {
+    Serial.println("Could not find valid BME280 sensor, check wiring!");
+    while (1);
+  }
 
- writeStationWelcome();
- delay(2000);
+  tcaselect(2);
+  display.clearDisplay();
+  display.display();
+  delay(3000);
 
+  writeStationWelcome();
+  delay(2000);
 }
 
 void loop() {
-  // read sensor on channel 3 (0x77)
-  tcaselect(3); 
+  tcaselect(3);
   Serial.println("Reading BME 280 on channel 3");
 
   float tempC = bme.readTemperature();
-  float tempF = tempC * (9.0 / 5.0) + 32.0; //convert celsius to fahrenheit
-  float pressure_hPa = bme.readPressure() / 100.0F; 
-  float pressure_inHg = pressure_hPa * 0.02953; //convert hPa to inHg (inches of Mercury) 
+  float tempF = tempC * (9.0 / 5.0) + 32.0;
+  float pressure_hPa = bme.readPressure() / 100.0F;
+  float pressure_inHg = pressure_hPa * 0.02953;
   float humidity = bme.readHumidity();
 
   Serial.print("Temp: ");
@@ -79,47 +57,10 @@ void loop() {
   Serial.print(humidity);
   Serial.println("%");
 
-  //Update display on channel 2
   Serial.println("Writing OLED on channel 2");
   Serial.println();
 
   writeWeatherStationDisplay(tempF, pressure_hPa, humidity);
- 
+
   delay(5000);
 }
-
-void writeWeatherStationDisplay(float tempF, float pressure_hPa, float humidity) {
-  tcaselect(2);
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(1);
-  display.setTextColor(SSD1309_PIXEL_ON);
-
-  display.print("Temp = ");
-  display.print(tempF);
-  display.println(" *F");
-  display.println();
-
-  display.print("Press. = ");
-  display.print(pressure_hPa);
-  display.println(" hPa");
-  display.println();
-
-  display.print("Humid. = ");
-  display.print(humidity);
-  display.println("%");
-
-  display.display();
-}
-
-void writeStationWelcome() {
- tcaselect(2);
- display.clearDisplay();
- display.setCursor(15, 10);
- display.setTextSize(2);
- display.setTextColor(SSD1309_PIXEL_ON);
- display.print("Welcome!");
-
- display.display();
-}
-
